8ba5d7d6fd3d5714da2fcb636b37fceb7ebea8fd
JonasDauster <69653143+JonasDauster@users.noreply.github.com>
2020-08-14 00:32:57 +0200

    Create DataLoad.py
    File to run the net on data

---
+++ DataLoad.py
@@ -0,0 +1,125 @@
+#start this and also the CNN file with the same random seed or else the one hot encoding will be diffrent
+#in a command line interface you can do this with PYTHONHASHSEED=6 python3 nameofscript.py 
+# you can enter any number behind the hashseed, just stick to the same one for both files
+
+import numpy as np
+from keras.models import Sequential
+import tensorflow as tf
+import os
+from Bio import SeqIO
+from keras.preprocessing.text import text_to_word_sequence
+from keras.preprocessing.text import one_hot
+import pandas as pd
+from keras.preprocessing import sequence
+
+# load and preprocess#
+df = pd.read_csv("inserted_test.csv")
+#df.insert(2, 'New_ID', range(1, 1 + len(df)))
+print(df['sequence'])
+Ori = df['sequence'].tolist()
+Orilabel = df["seq_label"].tolist()
+from textwrap import wrap
+
+# cut to kmers
+kmer_size = 1
+#cut to kmers
+df['sequence'] = df.apply(lambda x: wrap(x['sequence'], kmer_size), axis=1)
+
+#to ensure keras one_hot encodes always the same
+# when starting script a Hash seed should be set so that the output is the same over mulitple files , set to fixed seed above
+# labels = df[['seq_label']].values.tolist()
+print(df.dtypes)
+print(df['sequence'])
+print(df['seq_label'].value_counts().sort_index())
+
+df['sequence'] = [','.join(map(str, l)) for l in df['sequence']]
+print("possible Form")
+print(df.head(100))
+max_length = df.sequence.map(lambda x: len(x)).max()
+print(max_length)
+max_length = max_length/kmer_size
+df['sequence'] = df.apply(lambda x: text_to_word_sequence(x['sequence'], split=','), axis=1)
+# sequence cut 1 is just the first 16mers of ervery sample, because it is almost impossible to process uneven data with keras
+# if the data is scaled beforehand there shoul dbe no need for this column(need to think about how we make the data even, and also shuffel it,also imbalenced classes can pose a problem)
+cut_off = 3
+df.loc[:, 'sequence_cut1'] = df.sequence.map(lambda x: x[0:3])
+df['sequence'] = df['sequence'].astype(str)
+df['sequence_cut1'] = df['sequence_cut1'].astype(str)
+# vocab_max is the number of possible words/sequences, multiplication by  1.3 is normally just applied for natrual languages could be removed
+vocab_max = 4 ** kmer_size
+vocab_max1 = 4 ** kmer_size
+print(vocab_max)
+print(vocab_max1)
+# integer encode the document
+df['sequence'] = df.apply(lambda x: one_hot(x['sequence'], round(vocab_max * 1, 3)), axis=1)
+df['sequence_cut1'] = df.apply(lambda x: one_hot(x['sequence_cut1'], round(vocab_max1)), axis=1)
+print(df['sequence'])
+#df['seq_label'] = pd.Categorical(df['seq_label'])
+#df[seq_'label'] = df.label.cat.codes
+from sklearn.utils import shuffle, compute_class_weight
+
+#shuffel gets some more accuracy
+#df = shuffle(df)
+#joined = Ori.merge(df, left_on='New_ID', right_on='New_ID')
+#joined.to_csv("/working2/rcug_lw/pythonProjects/mistgabel/Jonas/joined.csv")
+
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+dataset = df.values
+Y = dataset[:,0]
+print(Y)
+encoder_label = LabelEncoder()
+encoder_label.fit(Y)
+encoded_Y = encoder_label.transform(Y)
+print(encoded_Y)
+dummy_y = np_utils.to_categorical(encoded_Y)
+
+target_softmax = dummy_y
+
+from sklearn.utils import class_weight
+#class_weights = class_weight.compute_class_weight('balanced',np.unique(Y),Y)
+print(df.head(100))
+max_lengthtest = 150
+train_numpy = pd.DataFrame(df['sequence_cut1'].values.tolist()).values
+#train_numpybig = pd.DataFrame(df['sequence'].values.tolist()).values
+train_numpybig = df["sequence"].values
+train_numpybig  = sequence.pad_sequences(train_numpybig,max_lengthtest,padding='post',truncating='post')
+#with np.printoptions(threshold=np.inf):
+   # print(train_numpybig, file=open('testout','a'))
+print(target_softmax)
+print(train_numpy)
+print(train_numpybig)
+print(train_numpybig[1])
+print(train_numpybig[1][1])
+print(train_numpybig[1:2])
+
+# load model
+from tensorflow import keras
+model = keras.models.load_model('modelconv.h5')
+model.summary()
+#scores = model.evaluate(train_numpybig,target_softmax, verbose=0)
+#print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+prediction = model.predict(train_numpybig)
+print(prediction)
+rnndf = []
+score = 0
+for i in range(len(Ori)):
+    print(Orilabel[i])
+    print(Ori[i])
+    print('%s => %d' % (train_numpybig[i].tolist(), prediction[i].argmax()))
+    print(prediction[i])
+    if encoded_Y[i] == prediction[i].argmax():
+        score=score+1
+    if prediction[i].argmax() == 0:
+        rnndf.append(Ori[i])
+    if prediction[i][prediction[i].argmax()] > 0.7:
+        print(prediction[i].argmax())
+    else:
+        print("no contamination")
+
+
+print(score/len(Ori))
+df=pd.DataFrame(rnndf)
+df.to_csv("/working2/rcug_lw/pythonProjects/mistgabel/Jonas/to_check.csv",index=False)
