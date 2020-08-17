25d73931a8bc74953bd58eb08bbfdedf158e33a6
JonasDauster <69653143+JonasDauster@users.noreply.github.com>
2020-08-17 23:28:38 +0200

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -19,9 +19,6 @@ kmer_size = 1
 #cut to kmers
 df['sequence'] = df.apply(lambda x: wrap(x['sequence'], kmer_size), axis=1)
 
-#to ensure keras one_hot encodes always the same
-# when starting script a Hash seed should be set so that the output is the same over mulitple files , set to fixed seed when starting via PHYTONSEED=0 and than phyton3 and programm
-
 print(df.dtypes)
 print(df['seq_label'].value_counts().sort_index())
 
@@ -32,22 +29,12 @@ max_length = df.sequence.map(lambda x: len(x)).max()
 print(max_length)
 max_length = max_length/kmer_size
 df['sequence'] = df.apply(lambda x: text_to_word_sequence(x['sequence'], split=','), axis=1)
-# sequence cut 1 is just the first 16mers of ervery sample, because it is almost impossible to process uneven data with keras
-# if the data is scaled beforehand there shoul dbe no need for this column(need to think about how we make the data even, and also shuffel it,also imbalenced classes can pose a problem)
-cut_off = 3
-df.loc[:, 'sequence_cut1'] = df.sequence.map(lambda x: x[0:3])
 df['sequence'] = df['sequence'].astype(str)
-df['sequence_cut1'] = df['sequence_cut1'].astype(str)
-# vocab_max is the number of possible words/sequences, multiplication by  1.3 is normally just applied for natrual languages could be removed
 vocab_max = 4 ** kmer_size
-vocab_max1 = 4 ** kmer_size
 print(vocab_max)
-print(vocab_max1)
 # integer encode the document
 df['sequence'] = df.apply(lambda x: one_hot(x['sequence'], round(vocab_max)), axis=1)
-df['sequence_cut1'] = df.apply(lambda x: one_hot(x['sequence_cut1'], round(vocab_max1)), axis=1)
-#df['seq_label'] = pd.Categorical(df['seq_label'])
-#df[seq_'label'] = df.label.cat.codes
+
 from sklearn.utils import shuffle, compute_class_weight
 
 #shuffel gets some more accuracy
@@ -66,14 +53,9 @@ dummy_y = np_utils.to_categorical(encoded_Y)
 target_softmax = dummy_y
 
 from sklearn.utils import class_weight
-#class_weights = class_weight.compute_class_weight('balanced',np.unique(Y),Y)
 max_lengthtest = 150
-train_numpy = pd.DataFrame(df['sequence_cut1'].values.tolist()).values
-#train_numpybig = pd.DataFrame(df['sequence'].values.tolist()).values
 train_numpybig = df["sequence"].values
 train_numpybig  = sequence.pad_sequences(train_numpybig,max_lengthtest,padding='post',truncating='post')
-#with np.printoptions(threshold=np.inf):
-   # print(train_numpybig, file=open('testout','a'))
 print(target_softmax)
 print(train_numpybig)
 
